96596120c21cf7376618918de29d6112b9e370ed
thaisalmeida29 <thaisalmeida.galvao@gmail.com>
2023-05-23 20:56:32 -0300

    criando-um-quiz

--- quiz.py
+++ quiz.py
@@ -1,2 +1,15 @@
 print("Seja bem vindos ao programa de quiz da Thais")
 answer_user = input("Quer começar? (S/N)")
+
+if answer_user != "S":
+    quit()
+
+print("Começando...")    
+
+print("Quem desenvolveu o jogo Grand Theft Auto (GTA) ? \n (A) (Rockstar Games) \n (B) (Ubisoft) \n (C) (EA) \n ")
+answer1 = input(" Resposta: ")
+
+if answer1 == "A":
+    print("Correto!")
+else:
+    print("Incorreto!")    
